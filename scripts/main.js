58a483090c8dff661577cd2686a7b52fc745e9e2
Suryash Chakravarty <suryash.c@gmail.com>
2026-01-10 13:36:45 -0800

    edited to reference the new model

--- scripts/main.js
+++ scripts/main.js
@@ -49,65 +49,65 @@ scene.add( gridHelper );
 
 const perfMonitor = new PerformanceMonitor()
 
-// const loader = new GLTFLoader().setPath('models/piperack/');
-// loader.load('piperacks_lod_working_4_onlyhires.glb', (gltf) => {
-//     const mesh = gltf.scene;
-//     mesh.position.set(0,0,0);
-//     scene.add(mesh);
-// })
-    
-const loader = new GLTFLoader();
-
-loader.load('models/piperack/piperacks_lod_working_4.glb', (gltf) => {    
-    const gltfScene = gltf.scene;
-
-    const objects = new Map()
-
-    gltfScene.traverse((child) => {
-        if (child.isMesh) {
-            const [obj, res] = child.name.split(';')
-
-            if (!objects.has(obj)) {
-                objects.set(obj, new Map())
-                objects.get(obj).set(res, child)
-
-            } else {
-                objects.get(obj).set(res, child)
-            }
-        }
-    })
-
-    objects.forEach((res_map, object_id) => {
-        const lod = new THREE.LOD()
-
-        let hires_pos = res_map.get('hires').position
-        let worldPos = new THREE.Vector3();
-        let worldQuat = new THREE.Quaternion();
-        let worldScale = new THREE.Vector3();
-        
-        res_map.get('hires').updateMatrixWorld(true)
-
-        res_map.get('hires').matrixWorld.decompose(worldPos, worldQuat, worldScale);
-        
-        lod.position.copy(hires_pos)
-        lod.quaternion.copy(worldQuat);
-        lod.scale.copy(worldScale);
-
-        res_map.forEach((mesh, resolution) => {
-            mesh.position.set(0, 0, 0);
-            mesh.quaternion.set(0, 0, 0, 1);
-            mesh.scale.set(1, 1, 1);
-
-            if (resolution === 'hires') {
-                lod.addLevel(mesh, 0)
-            } else {
-                lod.addLevel(mesh, 5)
-            }
-        })
-
-        scene.add(lod)
-    })
+const loader = new GLTFLoader().setPath('models/bim-model/');
+loader.load('sixty5_100_piping_test_full.glb', (gltf) => {
+    const mesh = gltf.scene;
+    mesh.position.set(0,0,0);
+    scene.add(mesh);
 })
+    
+// const loader = new GLTFLoader();
+
+// loader.load('models/piperack/piperacks_lod_working_4.glb', (gltf) => {    
+//     const gltfScene = gltf.scene;
+
+//     const objects = new Map()
+
+//     gltfScene.traverse((child) => {
+//         if (child.isMesh) {
+//             const [obj, res] = child.name.split(';')
+
+//             if (!objects.has(obj)) {
+//                 objects.set(obj, new Map())
+//                 objects.get(obj).set(res, child)
+
+//             } else {
+//                 objects.get(obj).set(res, child)
+//             }
+//         }
+//     })
+
+//     objects.forEach((res_map, object_id) => {
+//         const lod = new THREE.LOD()
+
+//         let hires_pos = res_map.get('hires').position
+//         let worldPos = new THREE.Vector3();
+//         let worldQuat = new THREE.Quaternion();
+//         let worldScale = new THREE.Vector3();
+        
+//         res_map.get('hires').updateMatrixWorld(true)
+
+//         res_map.get('hires').matrixWorld.decompose(worldPos, worldQuat, worldScale);
+        
+//         lod.position.copy(hires_pos)
+//         lod.quaternion.copy(worldQuat);
+//         lod.scale.copy(worldScale);
+
+//         res_map.forEach((mesh, resolution) => {
+//             mesh.position.set(0, 0, 0);
+//             mesh.quaternion.set(0, 0, 0, 1);
+//             mesh.scale.set(1, 1, 1);
+
+//             if (resolution === 'hires') {
+//                 lod.addLevel(mesh, 0)
+//             } else {
+//                 lod.addLevel(mesh, 5)
+//             }
+//         })
+
+//         scene.add(lod)
+//     })
+// })
 
 function animate() {
     requestAnimationFrame(animate);
